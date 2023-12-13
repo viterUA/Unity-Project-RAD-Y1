598905dd408d606f6008dbc548153e76b438f78d
Mykhailo Hnylytskyi <mykhailohnylytskyi@gmail.com>
2023-12-13 12:42:47 +0000

    space war

--- Football simulitor/Assets/BomberGuyScript.cs
+++ Football simulitor/Assets/BomberGuyScript.cs
@@ -17,12 +17,6 @@ public class BomberGuyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
-               transform.Rotate(Vector3.up, -rotSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.W))
-            transform.position += currentSpeed * transform.forward * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.B))
             Instantiate(bombCLoneTemplate, transform.position, transform.rotation);
            
--- Football simulitor/Assets/CharacterControlScript.cs
+++ Football simulitor/Assets/CharacterControlScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class CharacterControlScript : MonoBehaviour
 {
+    Rigidbody rb;
     Vector3 velocity, acceleration;
     float rateOfRotation = 180f;
+    public float normalSpeed = 25f;
+    public float accelerationSpeed = 45f;
+    public float rotationSpeed = 2.0f;
+    float rotation = 0;
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
-  
+
 
     // Update is called once per frame
     void Update()
@@ -20,24 +27,31 @@ public class CharacterControlScript : MonoBehaviour
         acceleration = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
-        { acceleration = transform.forward; }
+        { rb.AddForce( transform.forward ); }
 
         if (Input.GetKey(KeyCode.S))
-        { acceleration = -transform.forward; }
+        { rb.AddForce(-transform.forward); }
 
-        if (Input.GetKey(KeyCode.D))
-        { transform.Rotate(Vector3.up, rateOfRotation * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotation = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotation = 1;
+        }
 
         if (Input.GetKey(KeyCode.UpArrow))
         { transform.Rotate(Vector3.right, rateOfRotation * Time.deltaTime); }
 
+        if (Input.GetKey(KeyCode.DownArrow))
+        { transform.Rotate(Vector3.left, rateOfRotation * Time.deltaTime); }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         { transform.Rotate(Vector3.forward, rateOfRotation * Time.deltaTime); }
 
-        velocity += acceleration * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+        { transform.Rotate(Vector3.back, rateOfRotation * Time.deltaTime); }
 
-
-        // s            =        u             *               t
-        transform.position += velocity * Time.deltaTime;
     }
 }
